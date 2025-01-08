9e155b3552343246edb9c9ffa017c2285b00db95
Harish Tadka <harishtadka@spartans-MacBook-Pro.local>
2025-01-08 19:21:16 +0530

    first commit

---
+++ admin/src/index.ts
@@ -0,0 +1,127 @@
+import { getTranslation } from './utils/getTranslation';
+import { PLUGIN_ID } from './pluginId';
+import { Initializer } from './components/Initializer';
+import { PluginIcon } from './components/PluginIcon';
+import {pluginPermissions} from './permissions';
+// @ts-ignore
+import { Alerts } from './components/Injected/Alerts/Alerts';
+// @ts-ignore
+import { ImportModal } from './components/ImportModal/ImportModal';
+// @ts-ignore
+import { ExportModal } from './components/ExportModal/ExportModal';
+import translations from './translations'; 
+// @ts-ignore
+import { InjectedImportExportSingleType } from './components/InjectedImportExportSingleType/InjectedImportExportSingleType';
+// @ts-ignore
+import { InjectedExportCollectionType } from './components/InjectedExportCollectionType/InjectedExportCollectionType';
+
+export default {
+  register(app: any) {
+    app.addMenuLink({
+      to: `plugins/${PLUGIN_ID}`,
+      icon: PluginIcon,
+      intlLabel: {
+        id: `${PLUGIN_ID}.plugin.name`,
+        defaultMessage:'Import Export',
+      },
+      permissions: pluginPermissions.main,
+      Component: async () => {
+        const { App } = await import('./pages/App');
+
+        return App;
+      },
+    });
+
+    app.registerPlugin({
+      id: PLUGIN_ID,
+      initializer: Initializer,
+      isReady: false,
+      name: PLUGIN_ID,
+    });
+  },
+
+  bootstrap(app:any) {
+    // app.injectContentManagerComponent('listView', 'actions', {
+    //   name: `${PLUGIN_ID}-alerts`,
+    //   Component: Alerts,
+    // });
+    app.getPlugin('content-manager').injectComponent('listView', 'actions', {
+      name: `${PLUGIN_ID}-alerts`,
+      Component: Alerts,
+    });
+    // app.injectContentManagerComponent('listView', 'actions', {
+    //   name: `${pluginId}-import`,
+    //   Component: ImportButton,
+    // });
+    // app.getPlugin('content-manager').injectComponent('listView', 'actions', {
+    //   name: `${PLUGIN_ID}-import`,
+    //   Component: ImportModal,
+    // });
+    // app.injectContentManagerComponent('listView', 'actions', {
+      //   name: `${pluginId}-export`,
+      //   Component: InjectedExportCollectionType,
+      // });
+      app.getPlugin('content-manager').injectComponent('listView', 'actions', {
+      name: `${PLUGIN_ID}-export`,
+      Component: InjectedExportCollectionType,
+    });
+      
+    // app.injectContentManagerComponent('editView', 'right-links', {
+    //   name: `${pluginId}-alerts`,
+    //   Component: Alerts,
+    // });
+    app.getPlugin('content-manager').injectComponent('editView', 'right-links', {
+      name: `${PLUGIN_ID}-alerts`,
+      Component: Alerts,
+    });
+    // app.injectContentManagerComponent('editView', 'right-links', {
+      //   name: `${pluginId}-import-export`,
+    //   Component: InjectedImportExportSingleType,
+    // });
+    app.getPlugin('content-manager').injectComponent('editView', 'right-links', {
+      name: `${PLUGIN_ID}-import-export`,
+      Component: InjectedImportExportSingleType,
+    });
+  },
+
+  async registerTrads(app: any) {
+    const { locales } = app;
+
+    const importedTranslations = [
+      {
+        data: translations.en,
+        locale: 'en'
+      },
+      {
+        data: translations.uk,
+        locale: 'uk'
+      }
+    ];
+
+    return importedTranslations;
+  },
+
+  // async registerTrads(app: any) {
+  //   const { locales } = app;
+
+  //   const importedTranslations = await Promise.all(
+  //     (locales as string[]).map((locale) => {
+  //       return import(`./translations/${locale}.json`)
+  //         .then(({ default: data }) => {
+  //           return {
+  //             data: getTranslation(data),
+  //             locale,
+  //           };
+  //         })
+  //         .catch(() => {
+  //           return {
+  //             data: {},
+  //             locale,
+  //           };
+  //         });
+  //     })
+  //   );
+
+  //   return importedTranslations;
+  // },
+};
---
+++ admin/src/pages/HomePage.tsx
@@ -0,0 +1,67 @@
+//@ts-nocheck
+import { Box, Checkbox, Flex, Link, Option, Select, Typography } from '@strapi/design-system';
+import React, { memo, useState } from 'react';
+import { Page } from '@strapi/strapi/admin';
+import { pluginPermissions } from '../permissions';
+import { Main } from '@strapi/design-system';
+import { useIntl } from 'react-intl';
+import { Header } from '../components/Header.jsx';
+import { ImportModal } from '../components/ImportModal/ImportModal.jsx';
+import { ExportModal } from '../components/ExportModal/ExportModal.jsx';
+import Preferences from '../components/Preferences/Preferences.jsx';
+import About from '../components/About/About.jsx';
+import { getTranslation } from '../utils/getTranslation';
+import { useI18n } from '../hooks/useI18n';
+import { dataFormats } from '../utils/dataFormats';
+
+const HomePage = () => {
+  const { formatMessage } = useIntl();
+  const { i18n } = useI18n();
+  return (
+    <>
+    {/* mango */}
+      <Main>
+        <Box padding={6} paddingTop={3}>
+          <Header />
+          <Page.Protect permissions={pluginPermissions.main}>
+            {/* <Box
+              style={{ alignSelf: 'stretch' }}
+              background="neutral0"
+              padding={8}
+              margin={6}
+              hasRadius={true}
+            >
+              <Flex direction="column" alignItems="start" gap={6}>
+                <Flex direction="column" alignItems="start" gap={0}>  
+                  <Typography variant="alpha">
+                    {i18n('plugin.page.homepage.section.quick-actions.title', 'Global Actions')}
+                  </Typography>
+                  <Typography variant="epsilon">
+                    {i18n('plugin.page.homepage.section.quick-actions.description', 'Import and export data from all your content types at once.')}
+                  </Typography>
+                </Flex>
+                <Box>
+                  <Flex direction="column" alignItems="start" gap={4}>
+                    <Flex gap={4}>
+                      <ImportModal />
+                      <ExportModal availableExportFormats={[dataFormats.JSON_V2]}/>
+                      <ExportButton availableExportFormats={[dataFormats.JSON_V2]} />
+                    </Flex>
+                  </Flex>
+                </Box>
+              </Flex>
+            </Box> */}
+            <Box padding={6} paddingTop={3} paddingBottom={0}>
+              <Preferences />
+            </Box>
+            <Box padding={6} paddingTop={3} paddingBottom={0}>
+              <About />
+            </Box>
+          </Page.Protect>
+        </Box>
+      </Main>
+    </>
+  );
+};
+
+export default memo(HomePage);
